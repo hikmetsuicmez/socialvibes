ea6c67752e64d079a4145f879b68f9d9baeacc45
Hikmet Suicmez <hikmet.suicmez1@gmail.com>
2024-02-28 01:37:28 +0300

    ModelMapper-configuration

--- src/main/java/com/thedxs/socialvibes/config/ApplicationConfig.java
+++ src/main/java/com/thedxs/socialvibes/config/ApplicationConfig.java
@@ -2,6 +2,7 @@ package com.thedxs.socialvibes.config;
 
 import com.thedxs.socialvibes.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -41,5 +42,10 @@ public class ApplicationConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
