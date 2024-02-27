fbb00ba31bbae4aba94d0b5e84ce5404af7d955c
Hikmet Suicmez <hikmet.suicmez1@gmail.com>
2024-02-27 18:56:58 +0300

    application.properties mysql configuration.

---
+++ src/main/java/com/thedxs/socialvibes/SocialvibesApplication.java
@@ -0,0 +1,13 @@
+package com.thedxs.socialvibes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialvibesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialvibesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thedxs/socialvibes/SocialvibesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thedxs.socialvibes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialvibesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
